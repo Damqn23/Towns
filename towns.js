7dc44d2eea6f6720db8b52d1e473e56484b0b534
Damqn23 <rauga2005@gmail.com>
2024-08-25 17:02:41 +0300

    Improved styling and effects for the functionality to add a new town

--- towns.js
+++ towns.js
@@ -4,21 +4,46 @@ $(document).ready(function() {
 	$('#btnAdd').click(addTown)
 });
 
+// Function to delete a town from the select element
 function deleteTown() {
-	let townName = $('#townName').val();
-	$('#townName').val('');
-	let removed = false;
-	for (let option of $('#towns option')) {
-		if (option.textContent == townName) {
-			removed = true;
-			option.remove();
-		}
-	}
-	if (removed)
-		$('#result').text(townName + " deleted.");
-	else
-		$('#result').text(townName + " not found.");
+    // Get the value from the input field
+    let townName = $('#townName').val();
+
+    // Clear the input field
+    $('#townName').val('');
+
+    // Flag to track if a town was removed
+    let removed = false;
+
+    // Iterate over each option in the select element
+    for (let option of $('#towns option')) {
+        // Check if the current option's text matches the townName
+        if (option.textContent === townName) {
+            // If found, set removed flag to true and remove the option
+            removed = true;
+            option.remove();
+        }
+    }
+
+    // Display an appropriate message based on whether the town was removed
+    if (removed) {
+        showMessage(townName + " deleted.");
+    } else {
+        showMessage(townName + " not found.");
+    }
 }
+
+// Function to display a message for a short time
+function showMessage(msg) {
+    // Set the text of the result element and make it visible
+    $('#result').text(msg).css("display", "block");
+
+    // Hide the message after 3 seconds with a 'blind' animation
+    setTimeout(function () {
+        $('#result').hide('blind', {}, 500);
+    }, 3000);
+}
+
 function addTown() { let townName = $('#townNameForAdd').val(); $('#townNameForAdd').val(''); $('#towns').append($('<option>').text(townName)); $('#result').text(townName + " added."); }
 function shuffleTowns() { let towns = $('#towns option').toArray(); $('#towns').empty();
 
